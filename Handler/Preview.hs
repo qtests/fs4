0dd162f8c40ed86875735c4aa9c458a7b535562a
qtests <qtests@outlook.com>
2018-07-10 14:34:57 +0200

    Image Previews

--- Handler/Preview.hs
+++ Handler/Preview.hs
@@ -23,18 +23,26 @@ import Foundation
 getPreviewR :: Int -> Handler Html
 getPreviewR ident = do
     -- StoredFile filename bytes <- getById fileid
-    StoredFile filename _ bytes <- getById ident
+    -- StoredFile filename _ bytes <- getById ident
+    StoredFile filename contentType bytes <- getById ident
+
     defaultLayout $ do
         setTitle . toMarkup $ "File Processor - " `Text.append` filename
-        previewBlock <- liftIO $ preview bytes
+        -- previewBlock <- liftIO $ preview bytes
+        previewBlock <- liftIO $ preview ident contentType bytes
         $(widgetFileNoReload def "preview")
 
-preview :: LB.ByteString -> IO Widget
-preview bytes = do
-    eText <- try . evaluate $ LT.decodeUtf8 bytes :: IO (Either SomeException LT.Text)
-    return $ case eText of
-      Left _ -> errorMessage
-      Right text -> [whamlet|<pre>#{text}|]
-  where
-    errorMessage = [whamlet|<pre>Unable to display file contents.|]
+-- preview :: LB.ByteString -> IO Widget
+-- preview bytes = do
+preview :: Int -> Text -> LB.ByteString -> IO Widget
+preview ident contentType bytes
+--    = do
+  | "image/" `Text.isPrefixOf` contentType = return [whamlet|<img src=@{DownloadR ident}>|]
+  | otherwise = do
+        eText <- try . evaluate $ LT.decodeUtf8 bytes :: IO (Either SomeException LT.Text)
+        return $ case eText of
+            Left _ -> errorMessage
+            Right text -> [whamlet|<pre>#{text}|]
+        where
+            errorMessage = [whamlet|<pre>Unable to display file contents.|]
 
